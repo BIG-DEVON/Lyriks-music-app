6735a16b96f1b1ef4bf9a7a8aa490528b90cfee3
BIG-DEVON <ezeajie14@gmail.com>
2023-01-21 09:40:17 +0100

    .

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -16,9 +16,23 @@ const TopChartCard = ({ song, i }) => (
   <div
     className="w-full flex flex-row items-center
    hover:bg-[#4c426e] py-2 p-4
-    rounded-lg cursor-pointer mb-2"
-  >
-    {song.title}
+    rounded-lg cursor-pointer mb-2">
+
+    <h3 className="font-bold text-base
+     text-white mr-3">{i +1}.</h3>
+     <div className="flex-1 flex flex-row 
+     justify-between items-center "> 
+        <img src={song?.images?.coverart } alt={song?.title}
+          className="w-20 h-20 rounded-lg " />
+          <div className="flex-1 flex flex-col 
+          justify-center mx-3">
+          <Link to={`/songs/${song.key}`}>
+          <p className="text-xl font-bold text-white">{song?.title}</p>
+
+          </Link>
+
+          </div>
+     </div>
   </div>
 );
 
@@ -80,14 +94,40 @@ const TopPlay = () => {
           <h2
             className="text-white font-bold
                text-2xl ">
-            Top Charts
+            Top Artists
           </h2>
-          <Link to="/top-charts">
+          <Link to="/top-artists">
             <p
               className="text-gray-300 
               text-base cursor-pointer">See more</p>
           </Link>
         </div>
+        <Swiper 
+        slidesPerView="auto"
+        spaceBetween={15}
+        freeMode
+        centeredSlides
+        centeredSlidesBounds
+        modules={[FreeMode ]}
+        className="mt-4 "
+        >
+        {topPlays?.map((song, i) => (
+          <SwiperSlide 
+          key={song?.key}
+          style={{ width: '25%', height: 'auto'}}
+          className='shadow-lg rounded-full
+           animate-slideright'  
+           
+           >
+            <Link to={`/artist/${song?.artists[0].adamid}`}>
+              <img src={song?.images.background} alt='name' 
+                className="rounded-full w-full object-cover"
+              />
+            </Link>
+          </SwiperSlide>
+        ))}
+          
+        </Swiper>
       
       </div>
      
