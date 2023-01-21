605abb66327f7c35c96ce98e20ab040c61b1748d
BIG-DEVON <ezeajie14@gmail.com>
2023-01-21 17:19:32 +0100

    ..

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -10,16 +10,16 @@ import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 import "swiper/css";
 import "swiper/css/free-mode";
-import { TopCharts } from "../pages";
 
-const TopChartCard = ({ song, i }) => (
+
+const TopChartCard = ({ song, i,isPlaying,activeSong,
+   handlePauseClick,handlePlayClick}) => (
   <div
-    className="w-full flex flex-row items-center
-   hover:bg-[#4c426e] py-2 p-4
-    rounded-lg cursor-pointer mb-2">
+    className={`w-full flex flex-row items-center 
+    hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} 
+    py-2 p-4 rounded-lg cursor-pointer mb-2`}>
 
-    <h3 className="font-bold text-base
-     text-white mr-3">{i +1}.</h3>
+    <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
      <div className="flex-1 flex flex-row 
      justify-between items-center "> 
         <img src={song?.images?.coverart } alt={song?.title}
@@ -27,12 +27,22 @@ const TopChartCard = ({ song, i }) => (
           <div className="flex-1 flex flex-col 
           justify-center mx-3">
           <Link to={`/songs/${song.key}`}>
-          <p className="text-xl font-bold text-white">{song?.title}</p>
-
+          <p className="text-xl font-bold
+           text-white">{song?.title}</p>
           </Link>
-
+          <Link to={`/artists/${song?.artists[0].adamid}`}>
+          <p className="text-base font-semibold
+           text-gray-300 mt-1">{song?.subtitle}</p>
+          </Link>
           </div>
      </div>
+     <PlayPause 
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      song={song}
+      handlePause={handlePauseClick}
+      handlePlay={handlePlayClick}
+     />
   </div>
 );
 
@@ -43,7 +53,7 @@ const TopPlay = () => {
   const divRef = useRef(null);
 
   useEffect(() => {
-    divRef.current.scrollIntoView({ behavior: "smooth" });
+    divRef.current.scrollIntoView({behavior: "smooth" });
   });
 
   const topPlays = data?.slice(0, 5);
@@ -52,7 +62,7 @@ const TopPlay = () => {
     dispatch(playPause(false));
   };
 
-  const handlePlayClick = () => {
+  const handlePlayClick = (song, i) => {
     dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true));
   };
@@ -60,8 +70,9 @@ const TopPlay = () => {
   return (
     <div
       ref={divRef}
-      className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 
-    xl:max-w-[500px] max-w-full flex flex-col "
+      className="xl:ml-6 ml-0 xl:mb-0 
+      mb-6 flex-1 xl:max-w-[500px]
+       max-w-full flex flex-col"
     >
       <div className="w-full flex flex-col">
         <div
@@ -69,9 +80,7 @@ const TopPlay = () => {
           items-center">
           <h2
             className="text-white font-bold
-               text-2xl ">
-            Top Charts
-          </h2>
+               text-2xl "> Top Charts   </h2>
           <Link to="/top-charts">
             <p
               className="text-gray-300 
@@ -80,20 +89,25 @@ const TopPlay = () => {
         </div>
         <div className="mt-4 flex flex-col gap-1">
           {topPlays?.map((song, i) => (
-            <TopChartCard key={song.key} song={song} i={i} />
+            <TopChartCard 
+            key={song.key} 
+            song={song} 
+            i={i}
+            isPlaying={isPlaying} 
+            activeSong={activeSong}
+            handlePauseClick={handlePauseClick}
+            handlePlayClick={() => handlePlayClick(song, i)} 
+
+            />
           ))}
         </div>
       </div>
 
-      <div className="w-full flex flex-col mt-8"> 
-      
-      
+      <div className="w-full flex flex-col mt-8">    
       <div
-          className="flex flex-row justify-between 
-          items-center">
+          className="flex flex-row justify-between items-center">
           <h2
-            className="text-white font-bold
-               text-2xl ">
+            className="text-white font-bold text-2xl ">
             Top Artists
           </h2>
           <Link to="/top-artists">
@@ -103,24 +117,22 @@ const TopPlay = () => {
           </Link>
         </div>
         <Swiper 
+        className="mt-4"
         slidesPerView="auto"
         spaceBetween={15}
         freeMode
         centeredSlides
         centeredSlidesBounds
         modules={[FreeMode ]}
-        className="mt-4 "
+        
         >
         {topPlays?.map((song, i) => (
           <SwiperSlide 
           key={song?.key}
           style={{ width: '25%', height: 'auto'}}
-          className='shadow-lg rounded-full
-           animate-slideright'  
-           
-           >
+          className='shadow-lg rounded-full animate-slideright' >
             <Link to={`/artist/${song?.artists[0].adamid}`}>
-              <img src={song?.images.background} alt='name' 
+              <img src={song?.images?.background} alt='Name' 
                 className="rounded-full w-full object-cover"
               />
             </Link>
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -10,22 +10,22 @@ const Discover = () => {
     const dispatch = useDispatch();
     const {activeSong, isPlaying} = useSelector((state) =>state.player);
     const { data, isFetching, error } = useGetTopChartsQuery();
-    const genreTile = 'Pop';
+    const genreTitle = 'Pop';
 
     if(isFetching) return <Loader title=" Loading your songs..."/>;
    
    if(error) return <Error />
     return (
-        <div className='flex flex-col '>
+        <div className='flex flex-col'>
             <div className='w-full flex justify-between items-center
          sm:flex-row flex-col mt-4 mb-10'>
                 <h2 className='font-bold text-3xl text-white
-                 text-left'> Discover {genreTile}</h2>
+                 text-left'> Discover {genreTitle}</h2>
                 <select
                     onChange={() => { }}
                     value=""
                     className='bg-black text-gray-300 p-3 text-sm 
-                rounded-lg   outline-none sm:mt-0 mt-5'
+                rounded-lg  outline-none sm:mt-0 mt-5'
 
                 >
                     {genres.map((genre) => <option key={genre.value}
