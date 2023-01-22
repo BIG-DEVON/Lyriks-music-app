a64441c38ac7edef3b4c2a88e2641f3d7e091310
BIG-DEVON <ezeajie14@gmail.com>
2023-01-23 00:20:27 +0100

    ..

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -2,28 +2,60 @@ import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
 import { setActiveSong, playPause } from "../redux/features/playerSlice";
+import { useGetSongDetailsQuery, useGetSongRelatedQuery } from "../redux/services/shazamCore";
+
 const SongDetails = () => {
     const dispatch = useDispatch();
     const { songid } = useParams();
     const { activeSong, isPlaying } = useSelector((state) => state.player);
-    console.log(songid);
+const { data:songData, isFetching: isFetchingSongDetails} =
+ useGetSongDetailsQuery({songid});
+ const {data, isFetching: isFetchingRelatedSongs, error } = useGetSongRelatedQuery({songid});
+if (isFetchingSongDetails || isFetchingRelatedSongs) return
+<Loader title="Searching song details"/>;
+
+const handlePauseClick = () => {
+    dispatch(playPause(false));
+
+  };
+  const handlePlayClick =(song , i) => {
+    dispatch(setActiveSong({song ,data, i}));
+    dispatch(playPause(true));
+  };
+
+if (error) return <Error />; 
 
     return (
         <div className="flex flex-col">
-        {/* <DetailsHeader 
-        artistId={artistId} 
+        <DetailsHeader 
+        artistId="" 
         songData={songData}
-
-        /> */}
+        />
 
             <div className="mb-10">
             <h2 className="text-white text-3xl font-bold ">
                 Lyrics:
             </h2>
-                <div className="mt-5">
+                <div className="mt-5 ">
+                {songData?.sections[1].type === 'LYRICS'  ? 
+                     songData?.sections[1].text.map((line, i) => ( 
+                    <p className="text-gray-300  my-1 font-serif text-xl">{line}</p>
+                )) :  <p className="text-gray-300 text-xl my-1 font-semibold">
+                Sorry, no lyrics found!😔</p>
+                
+                }
 
                 </div>
             </div>
+
+            <RelatedSongs 
+                data={data}
+                isPlaying={isPlaying}
+                activeSong={activeSong}
+                handlePauseClick={handlePauseClick}
+                handlePlayClick={handlePlayClick}
+
+             />
         </div>
     )
 
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -6,7 +6,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
     export const shazamCoreApi =createApi ({
         reducerPath: 'shazamCoreApi',
         baseQuery: fetchBaseQuery({ 
-            baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+            baseUrl: 'https://shazam-core.p.rapidapi.com/',
             prepareHeaders: (headers) => {
                 headers.set('X-RapidAPI-Key','d5bd53dda9msh8c18000aac164dep124706jsn1c18efd2c8da' );
                 
@@ -14,9 +14,15 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
             },
         }),
         endpoints: (builder) => ({
-            getTopCharts: builder.query({query: () =>  '/charts/world' }),
+            getTopCharts: builder.query({query: () => 'v1/charts/world' }),
+            getSongDetails: builder.query({query: ({songid}) =>  `v1/tracks/details?track_id=${songid}`}),
+            getSongRelated: builder.query({ query: ({songid}) => `v1/tracks/related?track_id=${songid}`}),
+            getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}`}),
         }),
     });
     export const {
         useGetTopChartsQuery,
+        useGetSongDetailsQuery,
+        useGetSongRelatedQuery,
+        useGetArtistDetailsQuery,
      } = shazamCoreApi;
